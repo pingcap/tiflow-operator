fa29e15cbe27a0299d311efa6f7736e5d12fc04b
Chunzhu Li <lichunzhu@stu.xjtu.edu.cn>
2022-08-19 11:11:39 +0800

    tiflow-master: support scaler and fix stsAnnotation bug (#16)
    * support master scaler, fix sts annotation bug

--- pkg/manager/member/scaler.go
+++ pkg/manager/member/scaler.go
@@ -1,8 +1,12 @@
 package member
 
 import (
+	"fmt"
+
 	apps "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/pingcap/tiflow-operator/api/v1alpha1"
 )
 
 const (
@@ -19,4 +23,8 @@ type Scaler interface {
 	ScaleOut(meta metav1.Object, actual *apps.StatefulSet, desired *apps.StatefulSet) error
 	// ScaleIn scales in the cluster
 	ScaleIn(meta metav1.Object, actual *apps.StatefulSet, desired *apps.StatefulSet) error
+}
+
+func ordinalPodName(memberType v1alpha1.MemberType, tcName string, ordinal int32) string {
+	return fmt.Sprintf("%s-%s-%d", tcName, memberType, ordinal)
 }
